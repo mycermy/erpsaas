34bb8d117bb6b02a845a41c5489e088ee2e44409
Zulfadli Resources <zulfadli.resources@gmail.com>
2025-10-15 05:48:33 +0800

    feat: Update currency defaults to MYR and adjust address factory and inventory seeder for Malaysian locations

--- config/money.php
+++ config/money.php
@@ -4,7 +4,7 @@ return [
 
     'defaults' => [
 
-        'currency' => env('MONEY_DEFAULTS_CURRENCY', 'USD'),
+        'currency' => env('MONEY_DEFAULTS_CURRENCY', 'MYR'),
 
         'convert' => env('MONEY_DEFAULTS_CONVERT', false),
 
